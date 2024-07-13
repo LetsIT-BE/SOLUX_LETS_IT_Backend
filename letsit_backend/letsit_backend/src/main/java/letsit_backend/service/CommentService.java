8ee5bc8f351855354999799eab9b74d3a7b16396
Yoon <ysj0509@sookmyung.ac.kr>
2024-07-14 06:12:48 +0900

    Feat: /Comment API 구현(댓글 생성, 삭제, 수정)
    - Controller : 각 메소드는 CommentResponseDto를 반환함.
      - 현재 생성 api 호출 시 json 입력 데이터로 [userId, comContent] 모두 넣어줘야 함. userId를 넣지 않는 방법은 없을까? 시큐리티 관련해서 알아봐야 할 듯.
      - 수정, 삭제 메소드의 uri가 지저분하다고 생각됨. commentId가 배타성을 지니고 있으니 postId를 빼도 될 듯함.
      - ResponseEntity를 커스텀하고 싶음.
    - CommentService :
      1: 매개변수의 postId를 통해 postRepository.findById(postId)로 게시글 존재여부 판단
      2 - a) upload: CommentRequestDto를 통해 userId를 get하여 memberRepository.findById 실행 -> 사용자 존재여부 판단
             request.toEntity로 comment 객체 생성. 생성한 걸 디비에 저장해 saveComment에 담은 후 CommentResponseDto로 감싸서 반환
      2 - b) updateComment: comment 존재 여부 판단 -> comment.getUserId().getUserId()로 멤버 겟 -> 유저아이디 컬럼 겟하여 댓글 수정권한 여부 판단
      2 - c) delete: 댓글 존재여부 판단 -> delete
    - CommentRequestDto : postId, userId, comContent, commentId / toEntity 매개변수로 post, member 객체
    - CommentResponseDto : userId, comContent, comCreateDate, comUpdateDate / comment 객체를 매개변수로 받는 생성자 통해 각 필드 할당

---
+++ letsit_backend/letsit_backend/src/main/java/letsit_backend/service/CommentService.java
@@ -0,0 +1,48 @@
+package letsit_backend.service;
+
+import letsit_backend.dto.CommentRequestDto;
+import letsit_backend.dto.CommentResponseDto;
+import letsit_backend.model.Comment;
+import letsit_backend.model.Member;
+import letsit_backend.model.Post;
+import letsit_backend.repository.CommentRepository;
+import letsit_backend.repository.MemberRepository;
+import letsit_backend.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class CommentService {
+    private final CommentRepository commentRepository;
+    private final MemberRepository memberRepository;
+    private final PostRepository postRepository;
+
+    public CommentResponseDto upload(Long postId, CommentRequestDto request) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다."));
+        Member member = memberRepository.findById(request.getUserId()).orElseThrow(() -> new IllegalArgumentException("사용자가 존재하지 않습니다."));
+
+        Comment comment = request.toEntity(post, member);
+        Comment savedComment = commentRepository.save(comment);
+        return new CommentResponseDto(savedComment);
+    }
+
+    public CommentResponseDto updateComment(Long postId, Long commentId, Long userId, CommentRequestDto request) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다."));
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new IllegalArgumentException("댓글이 존재하지 않습니다."));
+        if (!comment.getUserId().getUserId().equals(userId)) {
+            throw new IllegalArgumentException("댓글을 수정할 권한이 없습니다.");
+        }
+        comment.update(request.getComContent());
+
+        Comment updatedComment = commentRepository.save(comment);
+        return new CommentResponseDto(updatedComment);
+    }
+
+    public void delete(Long postId, Long commentId, Long userId) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new IllegalArgumentException("댓글이 존재하지 않습니다."));
+        commentRepository.delete(comment);
+    }
+}
