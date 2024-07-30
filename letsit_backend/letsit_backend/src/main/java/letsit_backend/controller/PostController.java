274951e89e229046d95d9fb3f0f78639d9819ed6
ChoiSeoY <chltjdud3002@gmail.com>
2024-07-30 15:27:10 +0900

    Fix: PostController Response 수정

--- letsit_backend/letsit_backend/src/main/java/letsit_backend/controller/PostController.java
+++ letsit_backend/letsit_backend/src/main/java/letsit_backend/controller/PostController.java
@@ -9,7 +9,6 @@ import letsit_backend.model.Member;
 import letsit_backend.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
