a8f5adabd837bde25a44ee4286d69bedd0afa685
EunSeo <gmjea023@naver.com>
2024-08-22 12:33:08 +0900

    feat: 글로벌예외처리

--- letsit_backend/letsit_backend/src/main/java/letsit_backend/jwt/CustomException.java
+++ letsit_backend/letsit_backend/src/main/java/letsit_backend/jwt/CustomException.java
@@ -3,20 +3,26 @@ package letsit_backend.jwt;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-//@Getter
-//@AllArgsConstructor
+@Getter
 public class CustomException extends RuntimeException {
 
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage()); // 예외 메시지로 ErrorCode의 메시지 사용
+        this.errorCode = errorCode;
+    }
     // private final ErrorCode errorCode;
     public CustomException(String message) {
         super(message);
+        this.errorCode = null;
     }
 
     public CustomException(String message, Throwable cause) {
         super(message, cause);
+        this.errorCode = null;
     }
-    /*
 
-     */
+
 }
 
---
+++ letsit_backend/letsit_backend/src/main/java/letsit_backend/jwt/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package letsit_backend.jwt;
+
+import letsit_backend.dto.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    protected ResponseEntity<Response<?>> handleDuplicateException(CustomException ex) {
+        ErrorCode errorCode = ex.getErrorCode();
+        return new ResponseEntity<>(Response.fail(errorCode.getMessage()), errorCode.getStatus());
+    }
+}
